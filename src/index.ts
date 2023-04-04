cdbeb916409eeb9165fa6e945a9b23343a2b004c
Huan Tran <huanductran98@gmail.com>
2023-04-04 01:42:00 -0700

    Complete add task feature

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import {v4 as uuidV4} from "uuid";
+
 // Use generic (<>) to specify the type of each element (apply for querySelector, not work for getElementById)
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem) // Render all the existing tasks in local storage
+
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createAt: Date}
 
 // If using getElementById, use 'as' to specify the types
 // const list = document.getElementById("list")  as HTMLUListElement | null
@@ -10,4 +20,44 @@ const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
 form?.addEventListener("submit", e => {
   e.preventDefault();
+
+  // Add ? (optional chaining) in case input is null because nothing with the id #new-task-title exists
+  if (input?.value === '' || input?.value === null) return
+
+  const newTask:Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createAt: new Date()
+  }
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ''
 })
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON === null) return []
+  return JSON.parse(taskJSON)
+}
